9f0039e4909fa0c2fbf5e9d94237411670e27390
Beni <benik@live.ca>
2022-09-28 18:00:52 -0400

    created a list of things i need to do

---
+++ todo.py
@@ -0,0 +1,92 @@
+
+
+#Break down
+
+
+#TODO Create a cvs list of data with words on one column the language you speak on the other language you want to learn.
+
+'''
+example:
+
+english,spanish
+hello,Hola
+how are you doing,cómo está
+
+'''
+
+#TODO Create a Gui for the program with PyQT and realease version thats downloadable on desktop for use
+
+# 1. ability to read data from csv (look at googlesheets) 
+'''
+can upload files from google sheet.
+can upload file from personal create csv
+can uploade file from text folder if the value are seprated the same way as csv with commas
+
+
+'''
+
+#2.Settings and Mode
+'''
+
+#### Settings #####
+Abilitity to choos the words you want in your learning list.
+Ability to choose the amount of word you want to learn.
+Ability to learn (words) , (phrase) or (alphabet)
+Ability to set timer to flip card
+Ability to go anki mode for daily learning
+Ability to set pomodoro technique WORK_MIN = 25 SHORT_BREAK_MIN = 5 LONG_BREAK_MIN = 20 how many session you want?
+Ability to set (image only) ,(text only), (image and text).....  to show up 
+    **** we can add the sound mode later on not main focus***
+
+
+
+
+
+
+
+######## Learning Mode #########
+
+
+
+random card ---------> 
+
+This mode show a random text 
+from the language you want to learn and you 
+can guess the word and click a button to 
+reveal the word in your own language
+
+
+random card + time ------------>
+
+This mode show's you a random text in the language 
+you want to learn but you have a certain time to think of the question
+before it flips the card and reveals the answer with two buttons asking you if you guess it 
+correct or no.
+
+if yes . remove the word on your list  | if no keep the word on your list
+
+
+order card -------------> 
+
+This show's the words in alphabetic order.
+from the language you want to learn and you 
+can guess the word and click a button to 
+reveal the word in your own language
+
+
+order card + time ----------->
+
+This show's the words in alphabetic order.
+in the language 
+you want to learn but you have a certain time to think of the question
+before it flips the card and reveals the answer with two buttons asking you if you guess it 
+correct or no.
+
+
+if yes . remove the word on your list  | if no keep the word on your list
+
+
+'''
+
+
+
